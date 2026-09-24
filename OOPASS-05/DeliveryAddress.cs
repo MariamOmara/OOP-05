a19410ecb4b60cc63dea87da485aa6827f991bae
Mariam Omara <mariamomara2005@gmail.com>
2026-09-24 22:47:29 +0300

    Q8 PR

--- OOPASS-05/DeliveryAddress.cs
+++ OOPASS-05/DeliveryAddress.cs
@@ -1099,3 +1099,348 @@
 //        DeliveryUtilities.PrintSeparator();
 //    }
 //}
+
+
+
+
+
+
+
+
+
+
+//Q8 PR
+
+//using System;
+
+//// =========================
+//// Q7 - Static Class
+//// =========================
+
+//public static class DeliveryUtilities
+//{
+//    public static void PrintSeparator()
+//    {
+//        Console.WriteLine("==========================================");
+//    }
+
+//    public static void PrintSystemTitle()
+//    {
+//        Console.WriteLine("Delivery Center");
+//    }
+//}
+
+
+//// =========================
+//// Delivery Address
+//// =========================
+
+//public class DeliveryAddress
+//{
+//    public string City { get; set; }
+//    public string Street { get; set; }
+//}
+
+
+//// =========================
+//// Shipment
+//// =========================
+
+//public class Shipment
+//{
+//    public int Id { get; set; }
+
+//    public DeliveryAddress DeliveryAddress { get; set; }
+
+//    // Q8 - Shipment Information
+//    public string TrackingCode { get; set; }
+//    public string ShipmentType { get; set; }
+//    public double Weight { get; set; }
+//    public string TrackingStatus { get; set; }
+
+
+//    // Q4 - Static Field
+//    public static int TotalShipmentsCreated = 0;
+
+
+//    // Q5 - Static Constructor
+//    static Shipment()
+//    {
+//        TotalShipmentsCreated = 0;
+//        Console.WriteLine("Shipment System Initialized");
+//    }
+
+
+//    // Normal Constructor
+//    public Shipment()
+//    {
+//        TotalShipmentsCreated++;
+//    }
+
+
+//    // Q6 - Static Method
+//    public static int GetTotalShipmentsCreated()
+//    {
+//        return TotalShipmentsCreated;
+//    }
+
+
+//    // Q1 - Object Copying
+//    public Shipment CopyShipment()
+//    {
+//        return new Shipment
+//        {
+//            Id = this.Id,
+//            DeliveryAddress = this.DeliveryAddress,
+
+//            TrackingCode = this.TrackingCode,
+//            ShipmentType = this.ShipmentType,
+//            Weight = this.Weight,
+//            TrackingStatus = this.TrackingStatus
+//        };
+//    }
+
+
+//    // Q2 - Shallow Copy
+//    public Shipment ShallowCopy()
+//    {
+//        return (Shipment)this.MemberwiseClone();
+//    }
+
+
+//    // Q3 - Deep Copy
+//    public Shipment DeepCopy()
+//    {
+//        return new Shipment
+//        {
+//            Id = this.Id,
+
+//            DeliveryAddress = new DeliveryAddress
+//            {
+//                City = this.DeliveryAddress.City,
+//                Street = this.DeliveryAddress.Street
+//            },
+
+//            TrackingCode = this.TrackingCode,
+//            ShipmentType = this.ShipmentType,
+//            Weight = this.Weight,
+//            TrackingStatus = this.TrackingStatus
+//        };
+//    }
+//}
+
+
+//// =========================
+//// Q8 - Extension Methods
+//// =========================
+
+//public static class ShipmentExtensions
+//{
+//    // Get shipment summary
+//    public static string GetSummary(this Shipment shipment)
+//    {
+//        return shipment.TrackingCode
+//            + " | "
+//            + shipment.ShipmentType
+//            + " | "
+//            + shipment.Weight
+//            + " KG"
+//            + " | "
+//            + shipment.TrackingStatus;
+//    }
+
+
+//    // Check if shipment is delivered
+//    public static bool IsDelivered(this Shipment shipment)
+//    {
+//        return shipment.TrackingStatus == "Delivered";
+//    }
+//}
+
+
+//// =========================
+//// Program
+//// =========================
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        // =========================
+//        // Q1 - Object Copying
+//        // =========================
+
+//        Shipment shipment1 = new Shipment
+//        {
+//            Id = 1,
+
+//            DeliveryAddress = new DeliveryAddress
+//            {
+//                City = "Cairo",
+//                Street = "Main Street"
+//            },
+
+//            TrackingCode = "SH001",
+//            ShipmentType = "Standard",
+//            Weight = 3,
+//            TrackingStatus = "In Transit"
+//        };
+
+//        Shipment shipment2 = shipment1;
+
+//        Shipment shipment3 = shipment1.CopyShipment();
+
+//        Console.WriteLine("Q1 - Object Copying");
+//        Console.WriteLine(shipment1 == shipment2);
+//        Console.WriteLine(shipment1 == shipment3);
+
+//        DeliveryUtilities.PrintSeparator();
+
+
+//        // =========================
+//        // Q2 - Shallow Copy
+//        // =========================
+
+//        Shipment shallowCopy = shipment1.ShallowCopy();
+
+//        Console.WriteLine("Q2 - Shallow Copy");
+
+//        Console.WriteLine(shipment1 == shallowCopy);
+
+//        Console.WriteLine(
+//            shipment1.DeliveryAddress == shallowCopy.DeliveryAddress
+//        );
+
+//        shallowCopy.DeliveryAddress.City = "Alexandria";
+
+//        Console.WriteLine(
+//            "Original City: " + shipment1.DeliveryAddress.City
+//        );
+
+//        Console.WriteLine(
+//            "Copied City: " + shallowCopy.DeliveryAddress.City
+//        );
+
+//        DeliveryUtilities.PrintSeparator();
+
+
+//        // =========================
+//        // Q3 - Deep Copy
+//        // =========================
+
+//        Shipment deepCopy = shipment1.DeepCopy();
+
+//        Console.WriteLine("Q3 - Deep Copy");
+
+//        Console.WriteLine(
+//            "Original City: " + shipment1.DeliveryAddress.City
+//        );
+
+//        Console.WriteLine(
+//            "Copied City: " + deepCopy.DeliveryAddress.City
+//        );
+
+//        deepCopy.DeliveryAddress.City = "Giza";
+
+//        Console.WriteLine("After changing copied address:");
+
+//        Console.WriteLine(
+//            "Original City: " + shipment1.DeliveryAddress.City
+//        );
+
+//        Console.WriteLine(
+//            "Copied City: " + deepCopy.DeliveryAddress.City
+//        );
+
+//        Console.WriteLine(
+//            "Same DeliveryAddress object? "
+//            + (shipment1.DeliveryAddress == deepCopy.DeliveryAddress)
+//        );
+
+//        DeliveryUtilities.PrintSeparator();
+
+
+//        // =========================
+//        // Q4 - Static Field
+//        // =========================
+
+//        Shipment shipment4 = new Shipment
+//        {
+//            Id = 4,
+//            DeliveryAddress = new DeliveryAddress
+//            {
+//                City = "Cairo",
+//                Street = "Street 1"
+//            }
+//        };
+
+//        Shipment shipment5 = new Shipment
+//        {
+//            Id = 5,
+//            DeliveryAddress = new DeliveryAddress
+//            {
+//                City = "Giza",
+//                Street = "Street 2"
+//            }
+//        };
+
+//        Console.WriteLine("Q4 - Static Field");
+
+//        Console.WriteLine(
+//            "Total Shipments Created: "
+//            + Shipment.TotalShipmentsCreated
+//        );
+
+//        DeliveryUtilities.PrintSeparator();
+
+
+//        // =========================
+//        // Q5 - Static Constructor
+//        // =========================
+
+//        Console.WriteLine("Q5 - Static Constructor");
+
+//        Console.WriteLine("Shipment System Initialized");
+
+//        DeliveryUtilities.PrintSeparator();
+
+
+//        // =========================
+//        // Q6 - Static Method
+//        // =========================
+
+//        Console.WriteLine("Q6 - Static Method");
+
+//        Console.WriteLine(
+//            "Total Shipments Created : "
+//            + Shipment.GetTotalShipmentsCreated()
+//        );
+
+//        DeliveryUtilities.PrintSeparator();
+
+
+//        // =========================
+//        // Q7 - Static Class
+//        // =========================
+
+//        Console.WriteLine("Q7 - Static Class");
+
+//        DeliveryUtilities.PrintSystemTitle();
+//        DeliveryUtilities.PrintSeparator();
+
+
+//        // =========================
+//        // Q8 - Extension Methods
+//        // =========================
+
+//        Console.WriteLine("Q8 - Extension Methods");
+
+//        // GetSummary()
+//        Console.WriteLine(shipment1.GetSummary());
+
+//        // IsDelivered()
+//        Console.WriteLine(
+//            "Is Delivered: " + shipment1.IsDelivered()
+//        );
+//    }
+//}
